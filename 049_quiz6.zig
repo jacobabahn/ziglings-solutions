f4f5c376ce9e650227c697d0d52478ed5dd6e20f
Jacob Bahn <jacobabahn@gmail.com>
2023-08-21 22:50:25 -0400

    Completed exercises through 65

--- 049_quiz6.zig
+++ 049_quiz6.zig
@@ -27,7 +27,13 @@ const Elephant = struct {
     // Your Elephant trunk methods go here!
     // ---------------------------------------------------
 
-    ???
+    pub fn getTrunk(self: *Elephant) *Elephant {
+        return self.trunk.?;
+    }
+
+    pub fn hasTrunk(self: *Elephant) bool {
+        if (self.trunk == null) return false else return true;
+    }
 
     // ---------------------------------------------------
 
